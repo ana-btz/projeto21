85d55703b1b8b946883841ab294fe7838ecda4e0
Ana Beatriz <silva.anabtz@gmail.com>
2024-01-02 12:52:04 -0300

    feat: implement /enrollments/cep correctly

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -26,7 +26,7 @@ export function handleApplicationErrors(
     });
   }
 
-  if (err.name === 'InvalidDataError') {
+  if (err.name === 'InvalidDataError' || err.name === 'InvalidCepError') {
     return res.status(httpStatus.BAD_REQUEST).send({
       message: err.message,
     });
