f17af48b41d86fe389857fbd23964e0f4642782f
Ana Beatriz <silva.anabtz@gmail.com>
2024-01-15 15:32:25 -0300

    feat: GET /hotels/:hotelId

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -9,3 +9,11 @@ export async function getHotels(req: AuthenticatedRequest, res: Response) {
     const hotels = await hotelsService.getHotels(userId);
     res.status(httpStatus.OK).send(hotels);
 }
+
+export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const hotelId = Number(req.params.hotelId);
+  
+    const hotelWithRooms = await hotelsService.getHotelsWithRooms(userId, hotelId);
+    res.status(httpStatus.OK).send(hotelWithRooms);
+  }
