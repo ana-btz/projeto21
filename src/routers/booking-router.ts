e08b4429db687694451cbbb01abc3631e33d9795
Ana Beatriz <silva.anabtz@gmail.com>
2024-01-21 07:24:59 -0300

    GET /booking

---
+++ src/routers/booking-router.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { authenticateToken } from "@/middlewares";
+import { getBookingByUserId } from "@/controllers";
+
+const bookingRouter = Router();
+
+bookingRouter
+    .all('/*', authenticateToken)
+    .get('/', getBookingByUserId)
+
+export { bookingRouter };
